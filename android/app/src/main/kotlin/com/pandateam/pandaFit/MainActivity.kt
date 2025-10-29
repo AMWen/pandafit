f07fec15a1d2ad0f576c1b4fec1b304d51987dcb
Amy Wen <wen.amy@gmail.com>
2025-10-29 11:14:29 -0400

    privacy policy and version fix

--- android/app/src/main/kotlin/com/pandateam/pandaFit/MainActivity.kt
+++ android/app/src/main/kotlin/com/pandateam/pandaFit/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.pandateam.pandaFit
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge for backward compatibility with Android 15+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+}
